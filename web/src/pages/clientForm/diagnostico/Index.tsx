4d47fea42f5fe72f367f12ec6f30d77d7aee9ee6
Gabriel Damico <gabrieldamico22@gmail.com>
2024-04-02 18:24:06 -0300

    refactor: Separação de accordions em arquivos separados

--- web/src/pages/clientForm/diagnostico/Index.tsx
+++ web/src/pages/clientForm/diagnostico/Index.tsx
@@ -1,34 +1,14 @@
-import { AddNewButton } from '@/components/AddNewButton'
+import ConcorrenteAccordion from '@/components/ConcorrenteAccordion'
 import CustomTable from '@/components/CustomTable'
 import DiscardDialog from '@/components/DiscardDialog'
 import SimpleList from '@/components/SimpleList'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion'
 import { AlertDialog } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
+import { Form } from '@/components/ui/form'
 import { Textarea } from '@/components/ui/textarea'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Trash } from '@phosphor-icons/react'
 import { useState } from 'react'
-import {
-  FormProvider,
-  useFieldArray,
-  useForm,
-  useFormContext,
-} from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { diagnosticoFormData, diagnosticoSchema } from './DiagnosticoSchema'
 
@@ -166,153 +146,5 @@ export default function DiagnosticoPage() {
       </div>
       <DiscardDialog />
     </AlertDialog>
-  )
-}
-
-const ConcorrenteAccordion = () => {
-  const { control } = useFormContext()
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: 'concorrentes',
-  })
-
-  const handleNewConcorrente = () => {
-    append({
-      nome: '',
-      redeSocial: '',
-      linkRedeSocial: '',
-      descricao: '',
-      pontosFortes: [{ value: '' }],
-      pontosFracos: [{ value: '' }],
-    })
-  }
-
-  const handleDeleteConcorrente = (index: number) => {
-    remove(index)
-  }
-
-  return (
-    <div className="flex flex-col gap-4 items-start justify-start">
-      <Accordion type="multiple" className="w-full flex flex-col gap-4">
-        {fields.map((field, index) => (
-          <div
-            key={field.id}
-            className="w-full flex flex-row gap-2 items-start justify-center"
-          >
-            <AccordionItem
-              className="flex-1 bg-zinc-100 px-2 rounded-lg shadow-sm space-y-4"
-              value={`item-${index}`}
-            >
-              <AccordionTrigger className="border-b-2 border-zinc-200 hover:cursor-pointer text-zinc-700 text-base px-2">
-                Concorrente {index + 1}
-              </AccordionTrigger>
-              <AccordionContent className="px-2">
-                <div className="flex-1 w-full xl:h-auto sm:h-full flex flex-col gap-8 ">
-                  <div className="w-full flex flex-row gap-8">
-                    <FormField
-                      name={`concorrentes.${index}.nome`}
-                      control={control}
-                      render={({ field, formState }) => (
-                        <FormItem className="w-full">
-                          <FormLabel htmlFor="nameInput">Nome</FormLabel>
-                          <FormControl>
-                            <Input
-                              id="nameInput"
-                              type="text"
-                              autoComplete="name"
-                              placeholder="Nome do Cliente"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      name={`concorrentes.${index}.redeSocial`}
-                      control={control}
-                      render={({ field, formState }) => (
-                        <FormItem className="w-full">
-                          <FormLabel htmlFor="redeSocialInput">
-                            Rede Social do concorrente
-                          </FormLabel>
-                          <FormControl>
-                            <Input
-                              id="redeSocialInput"
-                              type="text"
-                              placeholder="@Concorrente"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <FormField
-                    name={`concorrentes.${index}.linkRedeSocial`}
-                    control={control}
-                    render={({ field, formState }) => (
-                      <FormItem className="w-full">
-                        <FormLabel htmlFor="linkRedeSocialInput">
-                          Link da rede social do concorrente
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            id="linkRedeSocialInput"
-                            placeholder="http://url.com"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    name={`concorrentes.${index}.descricao`}
-                    control={control}
-                    render={({ field, formState }) => (
-                      <FormItem className="w-full">
-                        <FormLabel htmlFor="descricaoInput">
-                          Descrição
-                        </FormLabel>
-                        <FormControl>
-                          <Textarea
-                            id="descricaoInput"
-                            placeholder="Descrição do Concorrente"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <CustomTable
-                    pontosFortesName={`concorrentes.[${index}].pontosFortes`}
-                    pontosFracosName={`concorrentes.[${index}].pontosFracos`}
-                  />
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-            {index > 0 && (
-              <div className="flex items-center justify-center py-1">
-                <Button
-                  size="icon"
-                  variant={'ghost'}
-                  onClick={() => handleDeleteConcorrente(index)}
-                >
-                  <Trash
-                    size={24}
-                    weight="bold"
-                    className="text-destructive "
-                  />
-                </Button>
-              </div>
-            )}
-          </div>
-        ))}
-      </Accordion>
-      <AddNewButton onClick={handleNewConcorrente} />
-    </div>
   )
 }
--- web/src/pages/clientForm/projeto/index.tsx
+++ web/src/pages/clientForm/projeto/index.tsx
@@ -1,6 +1,7 @@
 import DiscardDialog from '@/components/DiscardDialog'
 import PalavraChaveInput from '@/components/PalavraChaveInput'
 import PersonasList from '@/components/PersonasList'
+import RedeSocialAccordion from '@/components/RedeSocialAccordion'
 import SimpleList from '@/components/SimpleList'
 import { AlertDialog } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
@@ -14,18 +15,10 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Link, Trash } from '@phosphor-icons/react'
+import { Link } from '@phosphor-icons/react'
 import { useState } from 'react'
-import { useFieldArray, useForm, useFormContext } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { projetoFormData, projetoSchema } from './ProjetoSchema'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion'
-import { Textarea } from '@/components/ui/textarea'
-import { AddNewButton } from '@/components/AddNewButton'
 
 export default function ProjetoPage() {
   const form = useForm<projetoFormData>({
@@ -316,142 +309,5 @@ export default function ProjetoPage() {
       </div>
       <DiscardDialog />
     </AlertDialog>
-  )
-}
-const RedeSocialAccordion = () => {
-  const { control } = useFormContext<projetoFormData>()
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: 'redesSociais',
-  })
-
-  const handleNewRede = () => {
-    append({
-      nome: '',
-      objetivo: '',
-      frequencia: '',
-      estruturaLinguagem: '',
-    })
-  }
-
-  const handleDeleteRede = (index: number) => {
-    remove(index)
-  }
-
-  return (
-    <div className="flex flex-col gap-4 items-start justify-start">
-      <Accordion type="multiple" className="w-full flex flex-col gap-4">
-        {fields.map((field, index) => (
-          <div
-            key={field.id}
-            className="w-full flex flex-row gap-2 items-start justify-center"
-          >
-            <AccordionItem
-              className="flex-1 bg-zinc-100 px-2 rounded-lg shadow-sm space-y-4"
-              value={`item-${index}`}
-            >
-              <AccordionTrigger className="border-b-2 border-zinc-200 hover:cursor-pointer text-zinc-700 text-base px-2">
-                Rede social {index + 1}
-              </AccordionTrigger>
-              <AccordionContent className="px-2">
-                <div className="flex-1 w-full xl:h-auto sm:h-full flex flex-col gap-8 ">
-                  <FormField
-                    name={`redesSociais.${index}.nome`}
-                    control={control}
-                    render={({ field, formState }) => (
-                      <FormItem className="w-full">
-                        <FormLabel htmlFor="nameInput">Nome</FormLabel>
-                        <FormControl>
-                          <Input
-                            id="nameInput"
-                            type="text"
-                            autoComplete="name"
-                            placeholder="Nome da rede social"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    name={`redesSociais.${index}.objetivo`}
-                    control={control}
-                    render={({ field, formState }) => (
-                      <FormItem className="w-full">
-                        <FormLabel htmlFor="objetivoInput">Objetivos</FormLabel>
-                        <FormControl>
-                          <Textarea
-                            id="objetivoInput"
-                            placeholder="Objetivos da Rede Social"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    name={`redesSociais.${index}.frequencia`}
-                    control={control}
-                    render={({ field, formState }) => (
-                      <FormItem className="w-full">
-                        <FormLabel htmlFor="redeSocialFrequencia">
-                          Frequencia
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            id="redeSocialFrequencia"
-                            type="text"
-                            placeholder="Frequencia de posts"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    name={`redesSociais.${index}.estruturaLinguagem`}
-                    control={control}
-                    render={({ field, formState }) => (
-                      <FormItem className="w-full">
-                        <FormLabel htmlFor="estruturaInput">
-                          Estratura de Linguagem
-                        </FormLabel>
-                        <FormControl>
-                          <Textarea
-                            id="estruturaInput"
-                            placeholder="Estrutura de linguagem da rede social"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-            {index > 0 && (
-              <div className="flex items-center justify-center py-1">
-                <Button
-                  size="icon"
-                  variant={'ghost'}
-                  onClick={() => handleDeleteRede(index)}
-                >
-                  <Trash
-                    size={24}
-                    weight="bold"
-                    className="text-destructive "
-                  />
-                </Button>
-              </div>
-            )}
-          </div>
-        ))}
-      </Accordion>
-      <AddNewButton onClick={handleNewRede} />
-    </div>
   )
 }
